87822c63c793f00efc50b7f4fa9a15a437a700b0
程序媛的故事.上集 <miss_zhao@ercun.me>
2019-02-27 19:53:54 +0800

    Add files via upload

---
+++ homework1/1-4/1-4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 0, num2 = 0;
+            Console.WriteLine("请输入第一个数字");
+            num1= double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个数字");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("两数的乘积为{0}",num1*num2);
+            Console.ReadKey();
+        }
+    }
+}
