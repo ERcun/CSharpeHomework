bdab4acbf3538ceb3940cf58588828d7b00fc91b
程序媛的故事.上集 <miss_zhao@ercun.me>
2019-03-19 02:30:22 +0800

    Add files via upload

---
+++ homework3/Abstract Factory Pattern/Abstract Factory Pattern/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory_Pattern
+{
+    class Program
+    {
+        //抽象工厂类：提供创建不同品牌的手机屏幕和手机主板
+        public abstract class AbstractFactory
+        {
+            //工厂生产屏幕
+            public abstract Screen CreateScreen();
+            //工厂生产主板
+            public abstract MotherBoard CreateMotherBoard();
+        }
+
+        //手机屏幕和主板基类
+        //屏幕抽象类：提供每一品牌的屏幕的继承
+        public abstract class Screen
+        {
+            public abstract void print();
+        }
+        //主板抽象类：提供每一品牌的主板的继承
+        public abstract class MotherBoard
+        {
+            public abstract void print();
+        }
+
+        //苹果手机屏幕和主板类
+        public class AppleScreen : Screen
+        {
+            public override void print()
+            {
+                Console.WriteLine("苹果手机屏幕！");
+            }
+        }
+        public class AppleMotherBoard : MotherBoard
+        {
+            public override void print()
+            {
+                Console.WriteLine("苹果手机主板！");
+            }
+        }
+
+        //小米手机屏幕和主板类
+        public class XiaoMiScreen : Screen
+        {
+            public override void print()
+            {
+                Console.WriteLine("小米手机屏幕！");
+            }
+        }
+        public class XiaoMiMotherBoard : MotherBoard
+        {
+            public override void print()
+            {
+                Console.WriteLine("小米手机主板！");
+            }
+        }
+
+        //苹果手机工厂类
+        public class AppleFactory : AbstractFactory
+        {
+            public override Screen CreateScreen()
+            {
+                return new AppleScreen();
+            }
+            public override MotherBoard CreateMotherBoard()
+            {
+                return new AppleMotherBoard();
+            }
+        }
+
+        //小米手机工厂类
+        public class XiaoMiFactory : AbstractFactory
+        {
+            public override Screen CreateScreen()
+            {
+                return new XiaoMiScreen();
+            }
+            public override MotherBoard CreateMotherBoard()
+            {
+                return new XiaoMiMotherBoard();
+            }
+        }
+
+
+        static void Main(string[] args)//测试代码
+        {
+            //小米工厂生产小米手机的屏幕和主板
+            AbstractFactory xiaomiFactory = new XiaoMiFactory();
+            Screen xiaomiScreen = xiaomiFactory.CreateScreen();
+            xiaomiScreen.print();
+            MotherBoard xiaomiMotherBoard = xiaomiFactory.CreateMotherBoard();
+            xiaomiMotherBoard.print();
+
+            //苹果工厂生产苹果手机屏幕和主板
+            AbstractFactory appleFactory = new AppleFactory();
+            Screen appleScreen = appleFactory.CreateScreen();
+            appleScreen.print();
+            MotherBoard appleMotherBoard = appleFactory.CreateMotherBoard();
+            appleMotherBoard.print();
+
+            Console.Read();
+        }
+    }
+}
---
+++ homework3/Simple Factory Pattern/Simple Factory Pattern/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Factory_Pattern
+{
+    class Program
+    {
+        public abstract class MobilePhone//手机类
+        {
+
+            public abstract void print();
+        }
+
+        public class Iphone : MobilePhone//苹果手机类
+        {
+
+            public override void print()
+            {
+                Console.WriteLine("我是苹果品牌！");
+            }
+        }
+
+        public class XiaoMI : MobilePhone//小米手机类
+        {
+            public override void print()
+            {
+                Console.WriteLine("我是小米手机");
+            }
+        }
+
+        public class SmarTisan : MobilePhone//锤子手机类
+        {
+            public override void print()
+            {
+                Console.WriteLine("我是锤子手机！");
+            }
+        }
+
+        public class MobielPhoneFactory//手机工厂类
+        {
+            public static MobilePhone CreateMobilePhone(string PhoneBrand)
+            {
+                MobilePhone mobilePhone = null;
+                if (PhoneBrand.Equals("苹果"))
+                {
+                    mobilePhone = new Iphone();
+                }
+                else if (PhoneBrand.Equals("小米"))
+                {
+                    mobilePhone = new XiaoMI();
+                }
+                else if (PhoneBrand.Equals("锤子"))
+                {
+                    mobilePhone = new SmarTisan();
+                }
+                else
+                {
+
+                }
+                return mobilePhone;
+            }
+
+        }
+
+        static void Main(string[] args)//测试代码
+        {
+            MobilePhone mobilephone1 = MobielPhoneFactory.CreateMobilePhone("苹果");
+            if (mobilephone1 != null)
+                mobilephone1.print();
+            MobilePhone mobilephone2 = MobielPhoneFactory.CreateMobilePhone("小米");
+            if (mobilephone2 != null)
+                mobilephone2.print();
+            MobilePhone mobilephone3 = MobielPhoneFactory.CreateMobilePhone("锤子");
+            if (mobilephone3 != null)
+                mobilephone3.print();
+            Console.Read();
+        }
+    }
+}
