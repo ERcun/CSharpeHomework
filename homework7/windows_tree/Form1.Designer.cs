52cd3f96d15631a5c72b1eadde84fc10be123ab9
程序媛的故事.上集 <miss_zhao@ercun.me>
2019-04-22 16:50:07 +0800

    Add files via upload

---
+++ homework7/windows_tree/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace windows_tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_du = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_number = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_du
+            // 
+            this.txt_du.Location = new System.Drawing.Point(676, 34);
+            this.txt_du.Name = "txt_du";
+            this.txt_du.Size = new System.Drawing.Size(100, 21);
+            this.txt_du.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(676, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(605, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "分叉的角度";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(641, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "颜色";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(641, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "数量";
+            // 
+            // txt_number
+            // 
+            this.txt_number.Location = new System.Drawing.Point(676, 123);
+            this.txt_number.Name = "txt_number";
+            this.txt_number.Size = new System.Drawing.Size(100, 21);
+            this.txt_number.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_number);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txt_du);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_du;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_number;
+    }
+}
+
---
+++ homework7/windows_tree/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_tree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //颜色集合
+            List<Color> list = new List<Color>() { Color.Red, Color.Blue,Color.Pink,Color.Black };
+            //下拉框绑定数据
+            comboBox1.DataSource = list;
+        }
+        private Graphics graphics;
+    
+        double perl = 0.6;
+        double per2 = 0.7;
+
+        void drawCayleyTree(int n,double x0, double y0, double leng, double th,Pen pen,double th1)
+        {
+            if (n == 0) return;
+
+            double xl = x0 + leng * Math.Cos(th);
+
+            double yl = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, xl, yl,pen);
+
+            drawCayleyTree(n - 1, xl, yl, perl * leng, th + th1,pen,th1);
+            drawCayleyTree(n - 1, xl, yl, per2 * leng, th - th1, pen,th1);
+        }
+
+        void drawLine(double x0, double y0, double x1, double y1,Pen pen)
+        {
+            graphics.DrawLine(
+            pen,
+            (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //判断是否为空
+            if (graphics != null)
+            {
+                //清空画板
+                graphics.Clear(Color.White);
+            }
+            //创建一个画板
+            graphics = this.CreateGraphics();
+            
+            Color c = (Color)comboBox1.SelectedItem;
+            Pen pen = new Pen(c);
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2, pen, ConvertDegreesToRadians(double.Parse(txt_du.Text)));
+        }
+        //角度换算弧度
+        public static double ConvertDegreesToRadians(double degrees)
+        {
+            double radians = (Math.PI / 180) * degrees;
+            return (radians);
+        }
+    }
+}
